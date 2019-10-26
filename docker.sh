68c0d2bc6a9170766445276e7bd93770ffa3f2a9
franciscochoi <fchoi@itba.edu.ar>
2019-10-26 18:22:37 -0300

    x

--- docker.sh
+++ docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-docker build . --tag $1/so_builder
+docker pull agodio/itba-so:1.0
 
-docker run -v ${PWD}:/sources/ $1/so_builder
+
+docker run --user $(id -u):$(id -g) -v ${PWD}:/mnt agodio/itba-so:1.0 /bin/bash -c "cd /mnt; cd Toolchain && make all && cd .. && make clean && make all"
 
 ./run.sh
