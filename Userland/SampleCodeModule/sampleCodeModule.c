85e354054e354128478849100c12680125b31988
franciscochoi <fchoi@itba.edu.ar>
2019-10-26 18:45:51 -0300

    va a andar

--- Userland/SampleCodeModule/sampleCodeModule.c
+++ Userland/SampleCodeModule/sampleCodeModule.c
@@ -2,7 +2,7 @@
 #include <syscall.h>
 
 uint64_t main() {
-	//uint64_t shellInfo = sys_create_process("shell", 1, shell);
-	//sys_run_process(shell, READY);
+	uint64_t shellInfo = sys_create_process("shell", 1, shell);
+	sys_run_process(shell, READY);
 	return 0;
 }
